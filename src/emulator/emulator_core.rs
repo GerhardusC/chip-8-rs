77a3667b824c52cc307de0fb8b9efa739dea4e4f
GerhardusC <gerhardus.cronje@rocketseed.com>
2026-06-03 15:23:33 +0200

    refactor: extracted large functions in big match statement

--- src/emulator/emulator_core.rs
+++ src/emulator/emulator_core.rs
@@ -89,43 +89,14 @@ impl<T: Draw, P: ReadInputState> Emulator<T, P> {
     fn execute(&mut self, instruction: Instruction) {
         match instruction {
             Instruction::ClearScreen => {
-                for i in self.screen_buffer.iter_mut() {
-                    *i = 0;
-                }
-                self.drawer.clear_screen();
+                self.clear_screen();
             }
             Instruction::Draw {
                 x_register,
                 y_register,
                 height,
             } => {
-                self.variable_registers[0xF] = 0;
-                let x_value = self.variable_registers[x_register] as u16;
-                let y_value = self.variable_registers[y_register] as u16;
-                let start_loc = y_value * SCREEN_WIDTH as u16 + x_value;
-
-                // For each row in the sprite
-                for i in 0..height {
-                    let sprite = self.memory[self.index_register + i as usize];
-
-                    let current_loc = start_loc + SCREEN_WIDTH as u16 * i as u16;
-
-                    // For each pixel in the row
-                    for j in 0..8 {
-                        let mask: u8 = 0b10000000 >> j;
-                        if let Some(x) = self.screen_buffer.get_mut(current_loc as usize + j) {
-                            let ans = mask & sprite;
-                            if *x > 0 && ans > 0 {
-                                *x = 0;
-                                self.variable_registers[0xF] = 1;
-                            } else if *x == 0 && ans > 0 {
-                                self.variable_registers[0xF] = 1;
-                                *x = if ans > 0 { 1 } else { 0 };
-                            }
-                        }
-                    }
-                }
-                self.drawer.draw_buffer(&self.screen_buffer);
+                self.draw(x_register, y_register, height);
             }
             Instruction::Jump(address) => self.program_counter = address as usize,
             Instruction::SetIndexRegister(address) => self.index_register = address as usize,
@@ -173,72 +144,7 @@ impl<T: Draw, P: ReadInputState> Emulator<T, P> {
                 register_x,
                 register_y,
             } => {
-                match operator {
-                    LogicalOperator::Set => {
-                        self.variable_registers[register_x] = self.variable_registers[register_y];
-                    }
-                    LogicalOperator::BinaryOr => {
-                        self.variable_registers[register_x] |= self.variable_registers[register_y];
-                    }
-                    LogicalOperator::BinaryAnd => {
-                        self.variable_registers[register_x] &= self.variable_registers[register_y];
-                    }
-                    LogicalOperator::LogicalXor => {
-                        self.variable_registers[register_x] ^= self.variable_registers[register_y];
-                    }
-                    LogicalOperator::AddAffectingCarry => {
-                        let res = self.variable_registers[register_x] as u16
-                            + self.variable_registers[register_y] as u16;
-                        self.variable_registers[register_x] = (res & 0xFF) as u8;
-                        self.variable_registers[0xF] = if res > 255 { 1 } else { 0 };
-                    }
-                    LogicalOperator::Subtract => {
-                        let res = self.variable_registers[register_x] as i16
-                            - self.variable_registers[register_y] as i16;
-                        self.variable_registers[register_x] = (res & 0xFF) as u8;
-                        self.variable_registers[0xF] = if res >= 0 { 1 } else { 0 };
-                    }
-                    LogicalOperator::SubtractReverse => {
-                        let res = self.variable_registers[register_y] as i16
-                            - self.variable_registers[register_x] as i16;
-                        self.variable_registers[register_x] = (res & 0xFF) as u8;
-                        self.variable_registers[0xF] = if res >= 0 { 1 } else { 0 };
-                    }
-                    LogicalOperator::Shift(direction) => {
-                        // TODO: Possible feature to optionally set VX to the value of VY
-                        match direction {
-                            Direction::Left => {
-                                // NEXT LINE ONLY IN QUIRKS
-                                self.variable_registers[register_x] =
-                                    self.variable_registers[register_y];
-
-                                let top = self.variable_registers[register_y] & 0b1000_0000;
-                                let res = self.variable_registers[register_y] << 1;
-                                self.variable_registers[register_x] = res;
-                                if top > 0 {
-                                    self.variable_registers[0xF] = 1
-                                } else {
-                                    self.variable_registers[0xF] = 0
-                                };
-                            }
-                            Direction::Right => {
-                                // NEXT LINE ONLY IN QUIRKS
-                                self.variable_registers[register_x] =
-                                    self.variable_registers[register_y];
-
-                                let bot = self.variable_registers[register_y] & 0b1;
-                                let res = self.variable_registers[register_y] >> 1;
-                                self.variable_registers[register_x] = res;
-                                if bot > 0 {
-                                    self.variable_registers[0xF] = 1
-                                } else {
-                                    self.variable_registers[0xF] = 0
-                                };
-                            }
-                        }
-                    }
-                    LogicalOperator::Invalid => {}
-                }
+                self.perform_logical_operator(register_x, register_y, operator);
             }
             Instruction::JumpWithOffset {
                 register_x,
@@ -266,101 +172,216 @@ impl<T: Draw, P: ReadInputState> Emulator<T, P> {
                 self.stack.push(self.program_counter);
                 self.program_counter = memory_addr;
             }
-            Instruction::FCommand { register, command } => match command {
-                FCommand::ReadDelayTimer => {
-                    self.variable_registers[register] =
-                        self.delay_timer.load(Ordering::Relaxed) as u8;
-                }
-                FCommand::SetDelayTimer => {
-                    self.delay_timer
-                        .store(self.variable_registers[register] as u16, Ordering::Relaxed);
-                }
-                FCommand::SetSoundTimer => {
-                    self.sound_timer
-                        .store(self.variable_registers[register] as u16, Ordering::Relaxed);
-                }
-                FCommand::AddToIndexRegister => {
-                    self.index_register += self.variable_registers[register] as usize;
-                }
-                FCommand::GetFontCharacter => {
-                    self.index_register =
-                        self.font_addr + self.variable_registers[register] as usize;
-                }
-                FCommand::DecimalConversion => {
-                    let val = self.variable_registers[register];
-                    let val = u8_to_arr(val);
-
-                    for (i, val) in val.iter().enumerate() {
-                        self.memory[self.index_register + i] = *val;
-                    }
-                }
-                FCommand::LoadFrom => {
-                    for (i, reg) in self.memory
-                        [self.index_register..=(self.index_register + register)]
-                        .iter()
-                        .enumerate()
-                    {
-                        self.variable_registers[i] = *reg;
-                    }
-                }
-                FCommand::StoreTo => {
-                    for (i, reg) in self.variable_registers[0..=register].iter().enumerate() {
-                        self.memory[self.index_register + i] = *reg;
-                    }
-                }
-                FCommand::GetKey => {
-                    loop {
-                        let mut key_pressed = false;
-                        if let Ok(keys) = self.input_provider.read_keys_state() {
-                            for (i, key) in keys.iter().enumerate() {
-                                if *key > 0 {
-                                    self.variable_registers[register] = i as u8;
-                                    key_pressed = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if key_pressed {
-                            break;
-                        };
-                    }
-                    self.input_provider.reset_keys_state();
-                }
-                FCommand::Unimplemented(value) => {
-                    if let RunningMode::Debug = self.running_mode {
-                        eprintln!("COMMAND {value} UNIMPLEMENTED");
-                    }
-                }
-            },
+            Instruction::FCommand { register, command } => {
+                self.perform_f_command(command, register)
+            }
             Instruction::SkipIfKey {
                 register,
                 state_to_check,
             } => {
-                if let Ok(keys) = self.input_provider.read_keys_state() {
-                    let current_key_state =
-                        keys[(self.variable_registers[register] & 0xF) as usize];
-                    match state_to_check {
-                        KeyStateToCheck::IsPressed => {
-                            if current_key_state > 0 {
-                                self.program_counter += 2;
-                            }
-                        }
-                        KeyStateToCheck::NotPressed => {
-                            if current_key_state == 0 {
-                                self.program_counter += 2;
-                            }
-                        }
-                        KeyStateToCheck::Invalid => {
-                            if let RunningMode::Debug = self.running_mode {
-                                eprintln!("Unrecognised instruction.")
-                            }
-                        }
-                    }
-                };
+                self.skip_if_key(state_to_check, register);
             }
             Instruction::Unimplemented(_) => {}
             Instruction::Error(_) => {}
         }
+    }
+
+    fn skip_if_key(&mut self, state_to_check: KeyStateToCheck, register: usize) {
+        if let Ok(keys) = self.input_provider.read_keys_state() {
+            let current_key_state = keys[(self.variable_registers[register] & 0xF) as usize];
+            match state_to_check {
+                KeyStateToCheck::IsPressed => {
+                    if current_key_state > 0 {
+                        self.program_counter += 2;
+                    }
+                }
+                KeyStateToCheck::NotPressed => {
+                    if current_key_state == 0 {
+                        self.program_counter += 2;
+                    }
+                }
+                KeyStateToCheck::Invalid => {
+                    if let RunningMode::Debug = self.running_mode {
+                        eprintln!("Unrecognised instruction.")
+                    }
+                }
+            }
+        };
+    }
+
+    fn perform_f_command(&mut self, command: FCommand, register: usize) {
+        match command {
+            FCommand::ReadDelayTimer => {
+                self.variable_registers[register] = self.delay_timer.load(Ordering::Relaxed) as u8;
+            }
+            FCommand::SetDelayTimer => {
+                self.delay_timer
+                    .store(self.variable_registers[register] as u16, Ordering::Relaxed);
+            }
+            FCommand::SetSoundTimer => {
+                self.sound_timer
+                    .store(self.variable_registers[register] as u16, Ordering::Relaxed);
+            }
+            FCommand::AddToIndexRegister => {
+                self.index_register += self.variable_registers[register] as usize;
+            }
+            FCommand::GetFontCharacter => {
+                self.index_register = self.font_addr + self.variable_registers[register] as usize;
+            }
+            FCommand::DecimalConversion => {
+                let val = self.variable_registers[register];
+                let val = u8_to_arr(val);
+
+                for (i, val) in val.iter().enumerate() {
+                    self.memory[self.index_register + i] = *val;
+                }
+            }
+            FCommand::LoadFrom => {
+                for (i, reg) in self.memory[self.index_register..=(self.index_register + register)]
+                    .iter()
+                    .enumerate()
+                {
+                    self.variable_registers[i] = *reg;
+                }
+            }
+            FCommand::StoreTo => {
+                for (i, reg) in self.variable_registers[0..=register].iter().enumerate() {
+                    self.memory[self.index_register + i] = *reg;
+                }
+            }
+            FCommand::GetKey => {
+                loop {
+                    let mut key_pressed = false;
+                    if let Ok(keys) = self.input_provider.read_keys_state() {
+                        for (i, key) in keys.iter().enumerate() {
+                            if *key > 0 {
+                                self.variable_registers[register] = i as u8;
+                                key_pressed = true;
+                                break;
+                            }
+                        }
+                    }
+                    if key_pressed {
+                        break;
+                    };
+                }
+                self.input_provider.reset_keys_state();
+            }
+            FCommand::Unimplemented(value) => {
+                if let RunningMode::Debug = self.running_mode {
+                    eprintln!("COMMAND {value} UNIMPLEMENTED");
+                }
+            }
+        }
+    }
+
+    fn perform_logical_operator(
+        &mut self,
+        register_x: usize,
+        register_y: usize,
+        operator: LogicalOperator,
+    ) {
+        match operator {
+            LogicalOperator::Set => {
+                self.variable_registers[register_x] = self.variable_registers[register_y];
+            }
+            LogicalOperator::BinaryOr => {
+                self.variable_registers[register_x] |= self.variable_registers[register_y];
+            }
+            LogicalOperator::BinaryAnd => {
+                self.variable_registers[register_x] &= self.variable_registers[register_y];
+            }
+            LogicalOperator::LogicalXor => {
+                self.variable_registers[register_x] ^= self.variable_registers[register_y];
+            }
+            LogicalOperator::AddAffectingCarry => {
+                let res = self.variable_registers[register_x] as u16
+                    + self.variable_registers[register_y] as u16;
+                self.variable_registers[register_x] = (res & 0xFF) as u8;
+                self.variable_registers[0xF] = if res > 255 { 1 } else { 0 };
+            }
+            LogicalOperator::Subtract => {
+                let res = self.variable_registers[register_x] as i16
+                    - self.variable_registers[register_y] as i16;
+                self.variable_registers[register_x] = (res & 0xFF) as u8;
+                self.variable_registers[0xF] = if res >= 0 { 1 } else { 0 };
+            }
+            LogicalOperator::SubtractReverse => {
+                let res = self.variable_registers[register_y] as i16
+                    - self.variable_registers[register_x] as i16;
+                self.variable_registers[register_x] = (res & 0xFF) as u8;
+                self.variable_registers[0xF] = if res >= 0 { 1 } else { 0 };
+            }
+            LogicalOperator::Shift(direction) => {
+                // TODO: Possible feature to optionally set VX to the value of VY
+                match direction {
+                    Direction::Left => {
+                        // NEXT LINE ONLY IN QUIRKS
+                        self.variable_registers[register_x] = self.variable_registers[register_y];
+
+                        let top = self.variable_registers[register_y] & 0b1000_0000;
+                        let res = self.variable_registers[register_y] << 1;
+                        self.variable_registers[register_x] = res;
+                        if top > 0 {
+                            self.variable_registers[0xF] = 1
+                        } else {
+                            self.variable_registers[0xF] = 0
+                        };
+                    }
+                    Direction::Right => {
+                        // NEXT LINE ONLY IN QUIRKS
+                        self.variable_registers[register_x] = self.variable_registers[register_y];
+
+                        let bot = self.variable_registers[register_y] & 0b1;
+                        let res = self.variable_registers[register_y] >> 1;
+                        self.variable_registers[register_x] = res;
+                        if bot > 0 {
+                            self.variable_registers[0xF] = 1
+                        } else {
+                            self.variable_registers[0xF] = 0
+                        };
+                    }
+                }
+            }
+            LogicalOperator::Invalid => {}
+        }
+    }
+
+    fn draw(&mut self, x_register: usize, y_register: usize, height: u8) {
+        self.variable_registers[0xF] = 0;
+        let x_value = self.variable_registers[x_register] as u16;
+        let y_value = self.variable_registers[y_register] as u16;
+        let start_loc = y_value * SCREEN_WIDTH as u16 + x_value;
+
+        // For each row in the sprite
+        for i in 0..height {
+            let sprite = self.memory[self.index_register + i as usize];
+
+            let current_loc = start_loc + SCREEN_WIDTH as u16 * i as u16;
+
+            // For each pixel in the row
+            for j in 0..8 {
+                let mask: u8 = 0b10000000 >> j;
+                if let Some(x) = self.screen_buffer.get_mut(current_loc as usize + j) {
+                    let ans = mask & sprite;
+                    if *x > 0 && ans > 0 {
+                        *x = 0;
+                        self.variable_registers[0xF] = 1;
+                    } else if *x == 0 && ans > 0 {
+                        self.variable_registers[0xF] = 1;
+                        *x = if ans > 0 { 1 } else { 0 };
+                    }
+                }
+            }
+        }
+        self.drawer.draw_buffer(&self.screen_buffer);
+    }
+
+    fn clear_screen(&mut self) {
+        for i in self.screen_buffer.iter_mut() {
+            *i = 0;
+        }
+        self.drawer.clear_screen();
     }
 
     fn set_mem_block(&mut self, set: &[u8], start_addr: usize) -> Result<(), ApplicationError> {
